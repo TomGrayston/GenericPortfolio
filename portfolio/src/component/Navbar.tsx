7818ae659675ddf49a7faffc09f412330eaf749a
Tom Grayston <tomgrayston@gmail.com>
2022-04-20 20:18:16 +0100

    simplified navbar and added auto generating navbar pages

--- portfolio/src/component/Navbar.tsx
+++ portfolio/src/component/Navbar.tsx
@@ -3,32 +3,19 @@ import { Link, Outlet } from "react-router-dom";
 import Logo from "./Navbar/Logo";
 import PageLinks from "./Navbar/PageLinks";
 
-const pages: string[] = [
-    'Projects',
-    'Blog',
-    'Contact'
-]
-
-const pagesURLs: string[] = [
-    '/projects',
-    '/blog',
-    '/contact'
-]
-
 function Navbar() {
+
+    const pages = [
+        {pageName: 'Projects', pageURL: '/projects'},
+        {pageName: 'Blog', pageURL:  '/blog'},
+        {pageName: 'Contact', pageURL: '/contact'},
+    ]
+
     return (
         <div className="navbar">
-            <Logo displayText="Generic Programmer"/>
-            <nav className="navbar-links">
-                <ul>
-                    <Link to="/projects"><li className="link"><a>Projects</a></li></Link>
-                    <Link to="/blog"><li className="link"><a>Blog</a></li></Link>
-                    <Link to="/contact"><li className="link"><a>Contact</a></li></Link>
-                </ul>
-            </nav>
-
+            <Logo displayText="Generic Programmer" />
+            <PageLinks pages={pages} />
             <Outlet />
-
         </div>
     );
     }
