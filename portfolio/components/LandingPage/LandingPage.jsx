d9b0b8724ca71d861dff26a3cea8c2fbc7433b2c
Tom Grayston <tomgrayston@gmail.com>
2022-05-10 21:14:38 +0100

    Implement nextjs (#8)
    * slight folder structure change

---
+++ portfolio/components/LandingPage/LandingPage.jsx
@@ -0,0 +1,13 @@
+import Header from "./Header";
+import { Spacer } from "@chakra-ui/layout";
+
+const LandingPage = () => {
+    return(
+        <>
+            <Header/>
+            <Spacer/>
+        </>
+    );
+}
+
+export default LandingPage;
