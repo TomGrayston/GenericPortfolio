f015a75bf2f1fc896e3da7d092ec4c56008eefcf
Tom Grayston <tomgrayston@gmail.com>
2022-05-14 22:56:17 +0100

    created fake example project

--- portfolio/components/LandingPage/LandingPage.jsx
+++ portfolio/components/LandingPage/LandingPage.jsx
@@ -1,5 +1,5 @@
 import Header from "./Header";
-import FeaturedWork from "./FeaturedWork";
+import FeaturedWork from "./FeaturedWorkSection/FeaturedWork";
 
 const LandingPage = () => {
     return(
