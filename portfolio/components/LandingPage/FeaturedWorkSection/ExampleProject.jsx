ae71171904d1e6b4862b022221e73b30f160a610
Tom Grayston <tomgrayston@gmail.com>
2022-05-21 00:18:57 +0100

    project cards are now directly clickable and deleted learn more button

--- portfolio/components/LandingPage/FeaturedWorkSection/ExampleProject.jsx
+++ portfolio/components/LandingPage/FeaturedWorkSection/ExampleProject.jsx
@@ -1,8 +1,9 @@
-import { Container, VStack, Divider, LinkBox, LinkOverlay} from "@chakra-ui/react";
+import { Container, VStack, Divider, LinkBox, LinkOverlay, Modal, ModalOverlay, ModalContent, Text,
+    ModalHeader, ModalBody, ModalCloseButton, useDisclosure, HStack, Badge, ModalFooter} from "@chakra-ui/react";
 import { useColorModeValue } from "@chakra-ui/color-mode";
 
 const ExampleProject = ({ children }) => {
-
+    const { isOpen, onOpen, onClose } = useDisclosure();
     return(
         <>
             <LinkBox>
@@ -12,13 +13,44 @@ const ExampleProject = ({ children }) => {
                 borderRadius="lg"
                 alignItems="flex-start"
                 maxW="container.md"
+                _hover={{ bg: useColorModeValue("gray.200", "gray.600")}}
+                cursor="pointer"
                 >
-                    <VStack alignItems="flex-start" >
-                        {children}
-                    </VStack>
+                    <LinkOverlay onClick={onOpen} >
+                        <VStack alignItems="flex-start" >
+                            {children}
+                        </VStack>
+                    </LinkOverlay>
                 </Container>
             </LinkBox>
             <Divider/>
+
+            <Modal isOpen={isOpen} onClose={onClose} size="2xl" motionPreset='slideInBottom'>
+                <ModalOverlay />
+                <ModalContent>
+                    <ModalHeader>
+                        Modal Title
+                    </ModalHeader>
+                    <ModalCloseButton />
+                    <ModalBody>
+                        <HStack p="0 0 1rem 0">
+                            <Badge colorScheme="red" variant="solid" >
+                                React
+                            </Badge>
+                            <Badge colorScheme="red" variant="solid" >
+                                Next JS
+                            </Badge>
+                        </HStack>
+                        <Text fontSize="sm">
+                        Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
+                        consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet
+                        minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.<br/>
+                        </Text>
+                    </ModalBody>
+                    <ModalFooter>
+                    </ModalFooter>
+                </ModalContent>
+            </Modal>
         </>
     )
 }
--- portfolio/components/LandingPage/FeaturedWorkSection/FeaturedWork.jsx
+++ portfolio/components/LandingPage/FeaturedWorkSection/FeaturedWork.jsx
@@ -1,15 +1,10 @@
 import {
-    Container, Heading, VStack,
-    Button, Badge, HStack, Text,
-    Modal, ModalOverlay, ModalContent,
-    ModalHeader, ModalFooter, ModalBody,
-    ModalCloseButton, useDisclosure, Divider, Link, LinkOverlay
+    Container, Heading, VStack, Button, Badge, HStack, Text, Divider, Link, LinkOverlay
     } from '@chakra-ui/react';
 import ExampleProject from "./ExampleProject";
 import PlaceholderProject from "./PlaceholderProject";
 
 const FeaturedWork = () => {
-    const { isOpen, onOpen, onClose } = useDisclosure()
     return(
         <Container maxW="container.md" p="1rem 1 0 0">
             <VStack
@@ -17,18 +12,16 @@ const FeaturedWork = () => {
             justify="center"
             m={{ base: "1rem 0 1.2rem 0", md: "3rem 0 4rem 0" }}
             >
-                <Heading fontSize="xl">
-                    Porfolio Projects
+                <Heading fontSize="xl" >
+                    Portfolio Projects
                 </Heading>
 
                 <PlaceholderProject/>
                 <PlaceholderProject/>
-                
+
                 <ExampleProject>
                     <Heading size="md" noOfLines="1">
-                        <LinkOverlay onClick={onOpen} cursor="pointer" >
-                            Example React Project
-                        </LinkOverlay>
+                        Example React Project
                     </Heading>
                     <HStack>
                         <Badge colorScheme="red" variant="solid" >
@@ -39,48 +32,21 @@ const FeaturedWork = () => {
                         </Badge>
                     </HStack>
                     <Text fontSize="sm">
-                    Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
-                    consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet
-                    minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.....<br/>
-                    </Text>
-                    <Button size="xs" variant="ghost" alignSelf="flex-end" onClick={onOpen}> Learn More </Button>
-                </ExampleProject>
-
-                <Link href="/projects" passHref align="center"><Button size="sm" colorScheme="red" width="100%"> View More Projects </Button></Link>
-                <Divider/>
-
-                <Modal isOpen={isOpen} onClose={onClose} size="2xl" motionPreset='slideInBottom'>
-                    <ModalOverlay />
-                    <ModalContent>
-                    <ModalHeader>Modal Title</ModalHeader>
-                    <ModalCloseButton />
-                    <ModalBody>
-                        <HStack p="0 0 1rem 0">
-                            <Badge colorScheme="red" variant="solid" >
-                                React
-                            </Badge>
-                            <Badge colorScheme="red" variant="solid" >
-                                Next JS
-                            </Badge>
-                        </HStack>
-                        <Text fontSize="sm">
                         Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia
                         consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet. Amet
                         minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.<br/>
-                        </Text>
-                    </ModalBody>
+                    </Text>
+                </ExampleProject>
 
-                    <ModalFooter>
-                        <Button colorScheme='blue' mr={3} onClick={onClose}>
-                        Close
-                        </Button>
-                    </ModalFooter>
-                    </ModalContent>
-                </Modal>
+                <Link href="/projects" passHref align="center" >
+                    <Button size="sm" colorScheme="red" width="100%" textDecoration="none">
+                        View More Projects
+                    </Button>
+                </Link>
+                <Divider/>
 
             </VStack>
         </Container>
-
     );
 }
 
