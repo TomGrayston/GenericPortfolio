b2f3ece8ab105680ef59f3a213790cac54c602b6
AndyMcGN <66390686+AndyMcGN@users.noreply.github.com>
2022-05-22 16:44:52 +0200

    add link, adjust heading

--- portfolio/pages/projects.tsx
+++ portfolio/pages/projects.tsx
@@ -9,6 +9,7 @@ import {
     Divider,
     Flex,
 } from "@chakra-ui/react";
+import NextLink from "next/link";
 
 import PROJECTS from "../public/projectsData.json";
 import { ArrowBackIcon } from "@chakra-ui/icons";
@@ -36,8 +37,13 @@ const ProjectsPage = () => {
     return (
         <Container maxW="container.md" p="1rem 1 0 0">
             <Flex align="center" justify="space-between" py="5">
-                <h2>Portfolio Projects</h2>
-                <Button colorScheme="red" size="sm" pl="0"><ArrowBackIcon w="8" h="6"/>Back to Home Page</Button>
+                <Heading as='h2' size='md'>Portfolio Projects</Heading>
+                <NextLink href={"/"}>
+                    <Button colorScheme="red" size="sm" pl="0">
+                        <ArrowBackIcon w="8" h="6" />
+                        Back to Home Page
+                    </Button>
+                </NextLink>
             </Flex>
             {projects}
         </Container>
