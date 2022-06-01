6448a9fa09f2a8c6300d5deafc15455dc976c6a0
Tom Grayston <tomgrayston@gmail.com>
2022-06-01 15:48:49 +0100

    31 auto scroll to comment section (#37)
    * auto-scrolling implemented with skip-link

--- portfolio/components/LandingPage/ContactSection/ContactSection.jsx
+++ portfolio/components/LandingPage/ContactSection/ContactSection.jsx
@@ -7,42 +7,44 @@ const ContactSection = ({ children }) => {
     const toast = useToast()
 
     return(
-        <Container maxW="container.md" >
-            <VStack
-            m={{ base: "1rem 0 1.2rem 0", md: "3rem 0 4rem 0" }}
-            >
-                <Heading fontSize="xl" alignSelf="flex-start">
-                    Contact Me
-                </Heading>
-                <FormControl >
-                    <VStack
-                    alignItems=""
-                    >
-                        <HStack>
-                            <Input id="name" placeholder="Name" size="lg" variant="filled" />
-                            <Input id="email" placeholder="Email Address" size="lg" variant="filled"/>
-                        </HStack>
-                        <Input id="subject" placeholder="Subject" size="lg" variant="filled"/>
-                        <Textarea id="message" placeholder="Message..." size="lg" variant="filled"/>
-                    </VStack>
-                </FormControl>
-                <HStack alignSelf="flex-end">
-                    <Button
-                        leftIcon={<EmailIcon/>}
-                        colorScheme="red"
-                        onClick={() =>
-                            toast({
-                                title: 'Message Sent',
-                                description: "I will try to get back to you as fast as possible",
-                                status: 'success',
-                                duration: 1500
-                                })}
+        <a id="contact">
+            <Container maxW="container.md" >
+                <VStack
+                m={{ base: "1rem 0 1.2rem 0", md: "3rem 0 4rem 0" }}
+                >
+                    <Heading fontSize="xl" alignSelf="flex-start">
+                        Contact Me
+                    </Heading>
+                    <FormControl >
+                        <VStack
+                        alignItems=""
                         >
-                        Send Message
-                    </Button>
-                </HStack>
-            </VStack>
-        </Container>
+                            <HStack>
+                                <Input id="name" placeholder="Name" size="lg" variant="filled" />
+                                <Input id="email" placeholder="Email Address" size="lg" variant="filled"/>
+                            </HStack>
+                            <Input id="subject" placeholder="Subject" size="lg" variant="filled"/>
+                            <Textarea id="message" placeholder="Message..." size="lg" variant="filled"/>
+                        </VStack>
+                    </FormControl>
+                    <HStack alignSelf="flex-end">
+                        <Button
+                            leftIcon={<EmailIcon/>}
+                            colorScheme="red"
+                            onClick={() =>
+                                toast({
+                                    title: 'Message Sent',
+                                    description: "I will try to get back to you as fast as possible",
+                                    status: 'success',
+                                    duration: 1500
+                                    })}
+                            >
+                            Send Message
+                        </Button>
+                    </HStack>
+                </VStack>
+            </Container>
+        </a>
     )
 }
 
